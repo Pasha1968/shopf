759505fba18d10f720037dc37cfbec9b498e2923
Porti <Porti@DESKTOP-CS86RGS>
2020-12-16 23:58:41 +0200

    PAgesaddDB

---
+++ Shopf/Shopf/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,24 @@
+﻿using Shopf.Models.Data;
+using Shopf.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopf.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pageList;
+
+            using (DB db = new DB()) {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+                return View(pageList);
+        }
+    }
+}
---
+++ Shopf/Shopf/Models/Data/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shopf.Models.Data
+{
+    public class DB : DbContext
+    {
+        public DbSet<PagesDTO> Pages { get; set; }
+    }
+}
