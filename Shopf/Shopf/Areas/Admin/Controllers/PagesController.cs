1cc29bb5e00573af1fbe2934e17d4da7007cd650
Porti <Porti@DESKTOP-CS86RGS>
2020-12-17 00:35:55 +0200

    add page complete

--- Shopf/Shopf/Areas/Admin/Controllers/PagesController.cs
+++ Shopf/Shopf/Areas/Admin/Controllers/PagesController.cs
@@ -21,10 +21,52 @@ namespace Shopf.Areas.Admin.Controllers
                 return View(pageList);
         }
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
             
             return View();
+        }
+        // Post: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            // Чек валидности ->  переменная заголовка(Slug) -> Инит Page DTO -> дать заголовок и т.д -> save в БД
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (DB db = new DB()) {
+                string slug;
+                PagesDTO dto = new PagesDTO();
+                dto.Title = model.Title.ToUpper();
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title exist");
+                    return View(model);
+                }
+                else if (db.Pages.Any(x => x.Slug == model.Slug)) {
+                    ModelState.AddModelError("", "That Slug exist");
+                    return View(model);
+                }
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+            TempData["SM"] = "Page has been added";
+            return RedirectToAction("Index");
         }
     }
 }
