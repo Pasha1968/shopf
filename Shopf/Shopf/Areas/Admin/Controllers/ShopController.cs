7fc77c8d2ca44e399dad7a4a25830ab742f9b7e7
Porti <Porti@DESKTOP-CS86RGS>
2020-12-18 00:10:02 +0200

    category

---
+++ Shopf/Shopf/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shopf.Areas.Admin.Controllers
+{
+    public class ShopController
+    {
+    }
+}
