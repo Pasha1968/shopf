872f36a8c9dab4448fb0d058a06f95702f7b9a2a
Porti <Porti@DESKTOP-CS86RGS>
2020-12-17 05:40:23 +0200

    jq

--- Shopf/Shopf/App_Start/BundleConfig.cs
+++ Shopf/Shopf/App_Start/BundleConfig.cs
@@ -13,6 +13,7 @@ namespace Shopf
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui.js*"));
 
@@ -26,8 +27,8 @@ namespace Shopf
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/jquery-ui.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-ui.css"));
         }
     }
 }
