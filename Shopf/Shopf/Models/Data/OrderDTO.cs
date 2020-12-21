5147f6d264919b1a65cfdf4f21195c1f789a4e36
Porti <Porti@DESKTOP-CS86RGS>
2020-12-21 22:52:24 +0200

    Models for Orders

---
+++ Shopf/Shopf/Models/Data/OrderDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Shopf.Models.Data
+{
+    [Table("tblOrders")]
+    public class OrderDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Createdat { get; set; }
+        public string Status { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UserDTO Users { get; set; }
+
+    }
+}
