efda2d02560c3882c9785b21d9b3f8930253a827
Porti <Porti@DESKTOP-CS86RGS>
2020-12-21 11:25:58 +0200

    cart

---
+++ Shopf/Shopf/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using Shopf.Models.ViewModels.Cart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shopf.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CartPartial()
+        {
+            CartVM model = new CartVM();
+
+            int qty = 0;
+
+            decimal price = 0m;
+
+            if(Session["cart"]!= null)
+
+            return PartialView();
+        }
+    }
+}
